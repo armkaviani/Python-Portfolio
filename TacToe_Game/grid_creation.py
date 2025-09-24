e796ba4e1eb968316146382578d00be82e2aa5a7
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-24 22:16:09 +0200

    Add grid_board method to return the grid.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/grid_creation.py
+++ TacToe_Game/grid_creation.py
@@ -5,9 +5,9 @@ class GridCreation:
         # initialize grid when the object is created
         self.grid = [[" " for _ in range(3)] for _ in range(3)]
 
-   
-
-
+      
+    def grid_board(self):
+            return self.grid
 
 
 
