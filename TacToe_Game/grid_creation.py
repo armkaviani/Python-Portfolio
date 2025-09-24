c33f157b4800b71d2ff3e2dcfcedaf532f35ddf8
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-24 22:14:25 +0200

    Initialize 3x3 grid in __init__ instead of grid_board.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/grid_creation.py
+++ TacToe_Game/grid_creation.py
@@ -1,15 +1,11 @@
 
 class GridCreation:
 
-    def grid_board(self):
-        grid = []
-        for i in range(3):
-            row = []
-            for j in range(3):
-                row.append(" ")
-            grid.append(row)
-            
-        
+    def __init__(self):
+        # initialize grid when the object is created
+        self.grid = [[" " for _ in range(3)] for _ in range(3)]
+
+   
 
 
 
