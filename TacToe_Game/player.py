a8c59d0a675c07f4347cec626a1a7bd73283f1d4
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-26 22:09:04 +0200

    Add Player class with __init__ to input player1 and player2 names.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

---
+++ TacToe_Game/player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    def __init__(self):
+            self.player1 = input("Please enter name of player 1 : ")
+            self.player2 = input("Please enter name of player 2 : ")
+
+
