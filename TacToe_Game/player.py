f42f0feaf1e943b826b3d7ef35b3b9579211e3a3
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-26 22:10:34 +0200

    Add player_turn method to switch turns between player1 and player2.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/player.py
+++ TacToe_Game/player.py
@@ -5,3 +5,11 @@ class Player:
             self.player2 = input("Please enter name of player 2 : ")
 
 
+    def player_turn(self, turn_player1):
+        if turn_player1:
+            turn_player1 = False
+            print(f"It's {self.player2}'s turn")
+        else:
+            turn_player1 = True
+            print(f"It's {self.player1}'s turn")
+        return turn_player1
