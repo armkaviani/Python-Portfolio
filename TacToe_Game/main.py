fbfd7da9e091152f2b5ef4096b867dddd7dcc090
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-26 22:20:43 +0200

    Call player_turn once after creating Player object.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/main.py
+++ TacToe_Game/main.py
@@ -1,11 +1,16 @@
 from grid_creation import GridCreation
+from player import Player
 
 def main():
 
     grid_obj = GridCreation()
     print(grid_obj.grid_board())
-    
+
     grid_obj.print_grid()
+
+    player_obj = Player()
+    turn = True  # Start with player1
+    player_obj.player_turn(turn)
 
 if __name__ == "__main__":
     main()
