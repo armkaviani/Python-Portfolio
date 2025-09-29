cc5bb3bde10a516f8f16b2140cb3c9260b9ba0cf
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-29 21:51:43 +0200

    Update player_turn to announce which player's turn it is with their symbol.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/player.py
+++ TacToe_Game/player.py
@@ -7,9 +7,6 @@ class Player:
 
     def player_turn(self, turn_player1):
         if turn_player1:
-            turn_player1 = False
-            print(f"It's {self.player2}'s turn")
+            print(f"It's {self.player1}'s turn (X)")
         else:
-            turn_player1 = True
-            print(f"It's {self.player1}'s turn")
-        return turn_player1
+            print(f"It's {self.player2}'s turn (O)")
