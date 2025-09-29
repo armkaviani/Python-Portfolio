1931a4c1a942f9f180f2581c6bd18a504a9ad22e
Armaghan Kaviani <armaghankaviani@yahoo.com>
2025-09-29 22:05:16 +0200

    Add check_winner function to determine if a player has won.
    Signed-off-by: Armaghan Kaviani <armaghankaviani@yahoo.com>

--- TacToe_Game/player.py
+++ TacToe_Game/player.py
@@ -10,3 +10,20 @@ class Player:
             print(f"It's {self.player1}'s turn (X)")
         else:
             print(f"It's {self.player2}'s turn (O)")
+
+    
+    def check_winner(grid, symbol):
+        # Check rows
+        for row in grid:
+            if all(cell == symbol for cell in row):
+                return True
+        # Check columns
+        for col in range(3):
+            if all(grid[row][col] == symbol for row in range(3)):
+                return True
+        # Check diagonals
+        if all(grid[i][i] == symbol for i in range(3)):
+            return True
+        if all(grid[i][2 - i] == symbol for i in range(3)):
+            return True
+        return False
